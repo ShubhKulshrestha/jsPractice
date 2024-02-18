5cb151da87841546c6a326d459977a65a69f3f83
ShubhKulshrestha <shubhamkulshrestha5196@gmail.com>
2024-02-18 17:28:37 +0530

    add.js

--- add.js
+++ add.js
@@ -1,5 +1,15 @@
-let a = parseInt(prompt("Enter the first number:"));
+//add two numbers.
+/*let a = parseInt(prompt("Enter the first number:"));
 let b = parseInt(prompt("Enter the second number:"));
 let add;
 add = a + b;
 console.log(add);
+*/
+//add two numbers using function.
+let a = parseInt(prompt("Enter the first number:"));
+let b = parseInt(prompt("Enter the second number:"));
+
+function myFunction(a,b){
+    console.log(a + b);
+}
+myFunction(a,b);
