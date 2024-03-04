41a669efdbf0b30665ec28bbfdf241983f7460a5
ShubhKulshrestha <shubhamkulshrestha5196@gmail.com>
2024-03-04 22:38:39 +0530

    .\zeroPosNag.js

---
+++ zeroPosNag.js
@@ -0,0 +1,10 @@
+let a = parseInt(prompt("Enter the number"));
+if (a===0){
+    console.log("The number is zero");
+}
+else if (a > 0){
+    console.log("The number is positive");
+}
+else {
+    console.log("The number is Negative");
+}
