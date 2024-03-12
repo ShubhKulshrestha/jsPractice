bf8c69583d76c5db783b1030d83ad3459b3cda45
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-03-12 23:59:44 +0530

    Update CelsiusToFahrenheit.js

--- CelsiusToFahrenheit.js
+++ CelsiusToFahrenheit.js
@@ -1,4 +1,5 @@
- const celsius = prompt("Enter a celsius value:");
+//convert celsius to Fahrenheit 
+const celsius = prompt("Enter a celsius value:");
 const fahrenheit = (celsius*1.8)+32;
 
 console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
