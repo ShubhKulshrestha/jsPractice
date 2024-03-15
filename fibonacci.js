c39f3f30271ab49b4d7c955b242dfe2c65de6cbe
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-03-15 21:53:17 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,8 @@
-
+const number = parseInt(prompt('Enter the number of terms: '));
+let a = 0, b = 1, Fibonacci ;
+for (let i = 1; i <= number; i++) {
+    console.log(a);
+    Fibonacci = a + b;
+    a = b;
+    b = Fibonacci;
+}
