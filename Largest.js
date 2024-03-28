57d6410d0976fc737a260022c74b334b088da899
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-03-28 22:07:22 +0530

    Update Largest.js

--- Largest.js
+++ Largest.js
@@ -1,1 +1,14 @@
-
+let num1 = parseFloat(prompt("Enter first number: "));
+let num2 = parseFloat(prompt("Enter second number: "));
+let num3 = parseFloat(prompt("Enter third number: "));
+let largest;
+if(num1 >= num2 && num1 >= num3) {
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+}
+else {
+    largest = num3;
+}
+console.log("The largest number is ", largest);
