74610ba98307f17913b2f8539b7b57f879526b45
Shubham Kulshrestha <144726609+ShubhKulshrestha@users.noreply.github.com>
2024-07-29 22:28:17 +0530

    Update Looponobject.js

--- Looponobject.js
+++ Looponobject.js
@@ -1,1 +1,10 @@
-
+const student = { 
+    name: 'Shubham',
+    age: 20,
+    hobbies: 'Shubham',
+};
+for (let key in student) { 
+    let value;
+    value = student[key];
+  console.log(key + "-" +  value); 
+} 
